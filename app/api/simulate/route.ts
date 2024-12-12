f46164a2c094aef070c9af36e435d8fb9506e4ac
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-12-12 14:58:22 -0500

    Use env

--- app/api/simulate/route.ts
+++ app/api/simulate/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const { scenario } = await request.json();
-  console.log(scenario);
+  fetch(`${process.env.API_URL}/simulate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ scenario }),
+  });
   return NextResponse.json({ message: 'Simulation started' });
 }
