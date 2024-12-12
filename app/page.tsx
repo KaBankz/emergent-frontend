7bba0f2c395427f4754fea7c20cba32ef86c3405
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-12-12 11:27:02 -0500

    Add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,207 +1,48 @@
-'use client';
+import Image from 'next/image';
 
-import { AnimatePresence, motion } from 'framer-motion';
-import {
-  LiveKitRoom,
-  useVoiceAssistant,
-  BarVisualizer,
-  RoomAudioRenderer,
-  VoiceAssistantControlBar,
-  AgentState,
-  DisconnectButton,
-} from '@livekit/components-react';
-import { useCallback, useEffect, useState } from 'react';
-import { MediaDeviceFailure } from 'livekit-client';
-import type { ConnectionDetails } from './api/connection-details/route';
-import { NoAgentNotification } from '@/components/NoAgentNotification';
-import { CloseIcon } from '@/components/CloseIcon';
-import { useKrispNoiseFilter } from '@livekit/components-react/krisp';
-import { Button } from '@/components/Button';
-
-export default function Page() {
-  const [connectionDetails, updateConnectionDetails] = useState<
-    ConnectionDetails | undefined
-  >(undefined);
-  const [agentState, setAgentState] = useState<AgentState>('disconnected');
-  const [currentSimulation, setCurrentSimulation] = useState('Heart Attack');
-
-  const onConnectButtonClicked = useCallback(async () => {
-    // Generate room connection details, including:
-    //   - A random Room name
-    //   - A random Participant name
-    //   - An Access Token to permit the participant to join the room
-    //   - The URL of the LiveKit server to connect to
-    //
-    // In real-world application, you would likely allow the user to specify their
-    // own participant name, and possibly to choose from existing rooms to join.
-
-    const url = new URL(
-      process.env.NEXT_PUBLIC_CONN_DETAILS_ENDPOINT ??
-        '/api/connection-details',
-      window.location.origin
-    );
-    const response = await fetch(url.toString());
-    const connectionDetailsData = await response.json();
-    updateConnectionDetails(connectionDetailsData);
-  }, []);
-
+export default function Home() {
   return (
     <main
       data-lk-theme='default'
       className='h-full grid content-center bg-[var(--lk-bg)]'
     >
-      <div className='flex flex-col gap-2 items-center'>
+      <div className='flex flex-col gap-2 items-center pb-10'>
         <span className='text-white text-6xl font-bold leading-tight'>
           Emergent
         </span>
       </div>
-      <LiveKitRoom
-        token={connectionDetails?.participantToken}
-        serverUrl={connectionDetails?.serverUrl}
-        connect={connectionDetails !== undefined}
-        audio={true}
-        video={false}
-        onMediaDeviceFailure={onDeviceFailure}
-        onDisconnected={() => {
-          updateConnectionDetails(undefined);
-        }}
-        className='grid grid-rows-[2fr_1fr] items-center'
-      >
-        <SimpleVoiceAssistant onStateChange={setAgentState} />
-        <ControlBar
-          onConnectButtonClicked={onConnectButtonClicked}
-          agentState={agentState}
-        />
-        <RoomAudioRenderer />
-
-        <div className='flex flex-col gap-2 items-center'>
-          <span className='text-white text-2xl font-bold flex flex-col gap-2'>
-            Medical Emergencies Simulations
-          </span>
-          <div className='flex flex-row gap-2 justify-center'>
-            <Button
-              className={
-                currentSimulation === 'Heart Attack'
-                  ? 'bg-red-500 text-white hover:bg-red-600'
-                  : ''
-              }
-              onClick={() => {
-                fetch('/api/simulate', {
-                  method: 'POST',
-                  body: JSON.stringify({
-                    simulation: 'heart-attack',
-                  }),
-                });
-                setCurrentSimulation('Heart Attack');
-              }}
-            >
-              Heart Attack
-            </Button>
-            <Button
-              className={
-                currentSimulation === 'Stroke'
-                  ? 'bg-red-500 text-white hover:bg-red-600'
-                  : ''
-              }
-              onClick={() => {
-                fetch('/api/simulate', {
-                  method: 'POST',
-                  body: JSON.stringify({
-                    simulation: 'stroke',
-                  }),
-                });
-                setCurrentSimulation('Stroke');
-              }}
-            >
-              Stroke
-            </Button>
-          </div>
+      <div className='flex flex-row gap-8 items-center justify-center'>
+        <div className='flex flex-col gap-2 items-center rounded-md p-4 hover:scale-110 transition-all duration-300 bg-neutral-900 cursor-pointer'>
+          <Image
+            className='rounded-md object-cover w-[300px] h-[300px]'
+            src='/heartattack.jpg'
+            alt='Heart Attack'
+            width={300}
+            height={300}
+          />
+          <span className='font-semibold text-xl'>Heart Attack</span>
         </div>
-
-        <NoAgentNotification state={agentState} />
-      </LiveKitRoom>
+        <div className='flex flex-col gap-2 items-center rounded-md p-4 hover:scale-110 transition-all duration-300 bg-neutral-900 cursor-pointer'>
+          <Image
+            className='rounded-md object-cover w-[300px] h-[300px]'
+            src='/dizzy.png'
+            alt='Low Blood Pressure'
+            width={300}
+            height={300}
+          />
+          <span className='font-semibold text-xl'>Low Blood Pressure</span>
+        </div>
+        <div className='flex flex-col gap-2 items-center rounded-md p-4 hover:scale-110 transition-all duration-300 bg-neutral-900 cursor-pointer'>
+          <Image
+            className='rounded-md object-cover w-[300px] h-[300px]'
+            src='/cannotbreath.png'
+            alt='Low Blood Oxygen'
+            width={300}
+            height={300}
+          />
+          <span className='font-semibold text-xl'>Low Blood Oxygen</span>
+        </div>
+      </div>
     </main>
-  );
-}
-
-function SimpleVoiceAssistant(props: {
-  onStateChange: (state: AgentState) => void;
-}) {
-  const { state, audioTrack, agentTranscriptions } = useVoiceAssistant();
-
-  useEffect(() => {
-    props.onStateChange(state);
-  }, [props, state]);
-
-  return (
-    <div className='h-[300px] max-w-[90vw] mx-auto'>
-      {agentTranscriptions.map((transcription) => (
-        <div key={transcription.id}>{transcription.text}</div>
-      ))}
-      <BarVisualizer
-        state={state}
-        barCount={5}
-        trackRef={audioTrack}
-        className='agent-visualizer'
-        options={{ minHeight: 24 }}
-      />
-    </div>
-  );
-}
-
-function ControlBar(props: {
-  onConnectButtonClicked: () => void;
-  agentState: AgentState;
-}) {
-  /**
-   * Use Krisp background noise reduction when available.
-   * Note: This is only available on Scale plan, see {@link https://livekit.io/pricing | LiveKit Pricing} for more details.
-   */
-  const krisp = useKrispNoiseFilter();
-  useEffect(() => {
-    krisp.setNoiseFilterEnabled(true);
-  }, []);
-
-  return (
-    <div className='relative h-[100px]'>
-      <AnimatePresence>
-        {props.agentState === 'disconnected' && (
-          <motion.button
-            initial={{ opacity: 0, top: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, top: '-10px' }}
-            transition={{ duration: 1, ease: [0.09, 1.04, 0.245, 1.055] }}
-            className='uppercase absolute left-1/2 -translate-x-1/2 px-4 py-2 bg-white text-black rounded-md'
-            onClick={() => props.onConnectButtonClicked()}
-          >
-            Start a conversation
-          </motion.button>
-        )}
-      </AnimatePresence>
-      <AnimatePresence>
-        {props.agentState !== 'disconnected' &&
-          props.agentState !== 'connecting' && (
-            <motion.div
-              initial={{ opacity: 0, top: '10px' }}
-              animate={{ opacity: 1, top: 0 }}
-              exit={{ opacity: 0, top: '-10px' }}
-              transition={{ duration: 0.4, ease: [0.09, 1.04, 0.245, 1.055] }}
-              className='flex h-8 absolute left-1/2 -translate-x-1/2  justify-center'
-            >
-              <VoiceAssistantControlBar controls={{ leave: false }} />
-              <DisconnectButton>
-                <CloseIcon />
-              </DisconnectButton>
-            </motion.div>
-          )}
-      </AnimatePresence>
-    </div>
-  );
-}
-
-function onDeviceFailure(error?: MediaDeviceFailure) {
-  console.error(error);
-  alert(
-    'Error acquiring camera or microphone permissions. Please make sure you grant the necessary permissions in your browser and reload the tab'
   );
 }
